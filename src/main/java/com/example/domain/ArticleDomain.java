24d75e615592b2dee645295ec62d7a2637b3adb5
S.Kobayashi <shunsuke.kobayashi@rakus.co.jp>
2025-05-27 15:52:08 +0900

    Domainを追加

---
+++ src/main/java/com/example/domain/ArticleDomain.java
@@ -0,0 +1,15 @@
+package com.example.domain;
+
+/**
+ * 記事を表すドメイン.
+ */
+public class ArticleDomain {
+    /**投稿ID*/
+    private Integer id;
+
+    /**名前*/
+    private String name;
+
+    /**記事内容x*/
+    private String content;
+}
---
+++ src/main/java/com/example/domain/CommentDomain.java
@@ -0,0 +1,18 @@
+package com.example.domain;
+
+/**
+ * コメントを表すドメイン.
+ */
+public class CommentDomain {
+    /**コメントID*/
+    private Integer id;
+
+    /**名前*/
+    private String name;
+
+    /**コメント内容x*/
+    private String content;
+
+    /**投稿ID*/
+    private Integer article_id;
+}
